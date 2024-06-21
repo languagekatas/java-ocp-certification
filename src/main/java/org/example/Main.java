a205bc95c2fd7409dbf01f393b70cee1df322b9d
Alejandro Martînez <alex1983bcn@gmail.com>
2024-06-21 22:53:07 +0200

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,16 @@
 package org.example;
 
+/// This is the Main
+///
+/// ```java
+/// public class Main {
+///     public static void main(String[] args) {
+///         System.out.println("Hello world!");
+///     }
+/// }
+/// ```
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
